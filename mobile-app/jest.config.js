f5df1080906bdabd2f5d5027798207d2c57324a1
em3rc0dTh <eduardo.farid@thradex.com>
2026-08-20 21:39:51 -0500

    test: transform React Native Flow dependencies

--- mobile-app/jest.config.js
+++ mobile-app/jest.config.js
@@ -7,14 +7,15 @@ module.exports = {
     '<rootDir>/__tests__/**/*.test.ts',
     '<rootDir>/__tests__/**/*.test.tsx'
   ],
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'json', 'node'],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
   transform: {
     '^.+\\.tsx?$': ['ts-jest', {
       tsconfig: 'tsconfig.jest.json'
-    }]
+    }],
+    '^.+\\.jsx?$': '<rootDir>/scripts/jest-react-native-transformer.js'
   },
   transformIgnorePatterns: [
-    'node_modules/(?!(expo-crypto|expo-sqlite|expo-modules-core|@expo)/)'
+    'node_modules/(?!(react-native|@react-native|expo-crypto|expo-sqlite|expo-modules-core|@expo)/)'
   ],
   modulePathIgnorePatterns: [
     '<rootDir>/node_modules/'
