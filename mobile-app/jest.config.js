b148ce2273f343b5eee971c69d2771d126993f50
em3rc0dTh <eduardo.farid@thradex.com>
2026-08-20 21:42:17 -0500

    test: load React Native Jest globals

--- mobile-app/jest.config.js
+++ mobile-app/jest.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  setupFiles: ['<rootDir>/jest.setup.js'],
   testMatch: [
     '<rootDir>/src/**/*.test.ts',
     '<rootDir>/src/**/*.test.tsx',
